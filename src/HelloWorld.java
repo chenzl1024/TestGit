ef80a1bd372a129b3e51a50d5d37adedd4551115
chenzl <chenzlliberty@163.com>
2017-06-24 21:20:26 +0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/6/24 0024.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
